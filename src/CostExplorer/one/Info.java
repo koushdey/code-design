390e9753c7ec86055c55c35aade04e01c1b2d81d
Koushik Dey <57134947+koushdey@users.noreply.github.com>
2023-04-18 00:46:58 +0530

    Update Info.java

--- src/CostExplorer/one/Info.java
+++ src/CostExplorer/one/Info.java
@@ -4,4 +4,6 @@ import java.time.LocalDate;
 
 record Subscription(Plan plan, LocalDate starDate){};
 record Product(String Name, Subscription subscription){};
+//record Product(String Name, List<Subscription> subscriptions){};  //LEVEL 1
 record Customer(String Name, Product product){};
+//record Customer(String Name, List<Product> products){};   //LEVEL 2
