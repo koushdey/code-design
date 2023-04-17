b4f3aa470a5588612f07177b9196c9ff43616f9b
Koushik Dey <57134947+koushdey@users.noreply.github.com>
2023-04-18 00:43:52 +0530

    Update App.java

--- src/CostExplorer/one/App.java
+++ src/CostExplorer/one/App.java
@@ -11,9 +11,38 @@ public class App {
         Customer customer = new Customer("c1", 
         new Product("Jira", 
         new Subscription(Plan.PREMIUM, LocalDate.parse("2023-04-17"))));
+        /*LEVEL 1 Comment above line
+        Arrays.asList(
+            new Subscription(Plan.TRIAL, LocalDate.parse("2023-03-01")),
+            new Subscription(Plan.PREMIUM, LocalDate.parse("2023-04-17"))
+        )));
+        */
 
         CostExplorerImpl costExplorer = new CostExplorerImpl(customer);
         System.out.println(costExplorer.monthlyCost().toString());
         System.out.println(costExplorer.yearlyCost().toString());
+        
+        //LEVEL 2 Comment above lines
+        /*
+        Customer customer = new Customer("c1", 
+        Arrays.asList( 
+            new Product("Jira", 
+            Arrays.asList(
+                new Subscription(Plan.TRIAL, LocalDate.parse("2023-03-01")),
+                new Subscription(Plan.PREMIUM, LocalDate.parse("2023-04-17"))
+            )),
+            new Product("Confluence", 
+            Arrays.asList(
+                new Subscription(Plan.TRIAL, LocalDate.parse("2023-02-01")),
+                new Subscription(Plan.BASIC, LocalDate.parse("2023-03-21"))
+            ))
+        ));
+
+        CostExplorerImpl costExplorer = new CostExplorerImpl(customer);
+        System.out.println(costExplorer.monthlyCost().toString());
+        System.out.println(costExplorer.yearlyCostEstimate().toString());
+        System.out.println(costExplorer.yearlyCostPerProduct().toString());
+        */
+        
     }
 }
