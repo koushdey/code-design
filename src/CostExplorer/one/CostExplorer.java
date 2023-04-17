8de4704b619f598820bb598b9a751d686d0eded5
Koushik Dey <57134947+koushdey@users.noreply.github.com>
2023-04-18 00:45:01 +0530

    Update CostExplorer.java

--- src/CostExplorer/one/CostExplorer.java
+++ src/CostExplorer/one/CostExplorer.java
@@ -5,4 +5,6 @@ import java.util.List;
 public interface CostExplorer {
     List<Double> monthlyCost();
     Double yearlyCost();
+    //Map<String, Double> yearlyCostPerProduct();   //LEVEL 2
+    //Double yearlyCostEstimate();  //LEVEL 2
 }
