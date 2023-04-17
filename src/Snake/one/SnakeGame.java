89a97e5e47c8b20cb47b8479bd3010b4cb3d42f5
Koushik Dey <57134947+koushdey@users.noreply.github.com>
2023-04-17 21:05:46 +0530

    Update SnakeGame.java

--- src/Snake/one/SnakeGame.java
+++ src/Snake/one/SnakeGame.java
@@ -1,7 +1,8 @@
 package com.atlassian;
 
 public interface SnakeGame {
-    //void setDirection(Direction direction);
+    //void setDirection(Direction direction);  //LEVEL 1
+    //String render();  //LEVEL 2
     void moveSnake(Direction direction);
     boolean isGameOver();
 }
