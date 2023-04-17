bcb73ccd3ab558dd742ea0a8484e84c02ab6d8ec
Koushik Dey <57134947+koushdey@users.noreply.github.com>
2023-04-18 00:40:08 +0530

    Update CostExplorerImpl.java

--- src/CostExplorer/one/CostExplorerImpl.java
+++ src/CostExplorer/one/CostExplorerImpl.java
@@ -2,23 +2,39 @@ package CostExplorer.one;
 
 import java.text.DecimalFormat;
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+//import java.util.stream.*;  //LEVEL 2
 
 public class CostExplorerImpl implements CostExplorer {
 
-    //private Customer customer;
     private Plan plan;
     private LocalDate startDate;
+    //private List<Subscription> subscriptions;     //LEVEL 1 comment startDate & plan
+    
+    //private List<Product> products;	//LEVEL 2 comment above lines
+    //private Map<String, Double[]> pricingReportMap;
+    
     private Double[] prices;
+    
     private DecimalFormat df = new DecimalFormat("#.##");
 
     CostExplorerImpl(Customer customer){
-        //this.customer = customer;
         this.plan = customer.product().subscription().plan();
         this.startDate = customer.product().subscription().starDate();
+        //this.subscriptions = customer.product().subscriptions();    //LEVEL 1 comment startDate & plan
         prices = new Double[12];
         Arrays.fill(prices, 0.00);
+        
+        /* LEVEL 2 Comment all lines above
+        this.products = customer.products();	
+        pricingReportMap = new HashMap<>();	
+        for(Product product : products){	
+            Double[] prices = new Double[12];	
+            Arrays.fill(prices, 0.00);	
+            pricingReportMap.put(product.Name(), prices);	
+        }	
+        processBilling();
+        */
     }
 
     @Override
@@ -31,6 +47,40 @@ public class CostExplorerImpl implements CostExplorer {
             prices[i] = plan.getPrice();
         }
         return Arrays.asList(prices);
+        
+        /* LEVEL 1 comment above lines
+        Subscription billedSubscription;
+        Optional<Subscription> trialSub = subscriptions.stream().filter(o -> o.plan().equals(Plan.TRIAL)).findAny();
+        if(!trialSub.isPresent()){
+            billedSubscription = subscriptions.get(0);  
+        } else{
+            billedSubscription = subscriptions.get(1);
+        }
+        Plan billedPlan = billedSubscription.plan();
+        int month = billedSubscription.starDate().getMonthValue();
+        int day = billedSubscription.starDate().getDayOfMonth();
+        DecimalFormat df = new DecimalFormat("#.##");
+        prices[month-1]  = Double.valueOf(df.format(billedPlan.getPrice() - billedPlan.getPrice()/30*day));
+        for(int i=month; i < 12; i++){
+            prices[i] = billedPlan.getPrice();
+        }
+        return Arrays.asList(prices);
+        */
+        //-------------------------------//
+        /*  LEVEL 2 comment all lines in the method
+        Double[] monthlyBill = new Double[12];
+        Arrays.fill(monthlyBill, 0.00);
+
+        int index = 0;
+        while(index < 12){
+            for(Double[] price : pricingReportMap.values()){
+                monthlyBill[index] += price[index];
+            }
+            index++;
+        }
+        
+        return Arrays.asList(monthlyBill);
+        */
     }
 
     @Override
@@ -41,5 +91,50 @@ public class CostExplorerImpl implements CostExplorer {
         }
         return finalPrice;
     }
+    
+    // LEVEL 2 All methods below
+    /*
+    @Override
+    public Map<String, Double> yearlyCostPerProduct() {
+        Map<String, Double> result= pricingReportMap.entrySet().stream()
+                                    .collect(Collectors.toMap(e -> e.getKey(), e -> Arrays.asList(e.getValue()).stream().mapToDouble(Double::doubleValue).sum()));
+        //System.out.println(result);
+        return result;
+    }
+
+    @Override
+    public Double yearlyCostEstimate() {
+        
+        return pricingReportMap.values().stream()
+        .mapToDouble(
+            d -> Arrays.stream(d).mapToDouble(Double::doubleValue).sum())
+            .sum();
+    }
+
+    private void processBilling(){
+        for(Product product : products){
+            Subscription billedSubscription;
+
+            Optional<Subscription> trialSub = product.subscriptions().stream().filter(o -> o.plan().equals(Plan.TRIAL)).findAny();
+            if(!trialSub.isPresent()){
+                billedSubscription = product.subscriptions().get(0);  
+            } else{
+                billedSubscription = product.subscriptions().get(1);
+            }
+
+            Plan billedPlan = billedSubscription.plan();
+            int month = billedSubscription.starDate().getMonthValue();
+            int day = billedSubscription.starDate().getDayOfMonth();
+
+            DecimalFormat df = new DecimalFormat("#.##");
+            Double[] prices = pricingReportMap.get(product.Name());
+            prices[month-1]  = Double.valueOf(df.format(billedPlan.getPrice() - billedPlan.getPrice()/30*day));
+            for(int i=month; i < 12; i++){
+                prices[i] = billedPlan.getPrice();
+            }
+            pricingReportMap.put(product.Name(), prices);
+        }
+    }
+    */
     
 }
