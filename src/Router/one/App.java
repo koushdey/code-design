d3123f8f2a763ab6081415760cc2dbcd7ebd00b0
Koushik Dey <57134947+koushdey@users.noreply.github.com>
2023-04-18 00:16:58 +0530

    Update App.java

--- src/Router/one/App.java
+++ src/Router/one/App.java
@@ -6,6 +6,16 @@ public class App {
          RouterImpl  routerImpl = new RouterImpl();
          String result = routerImpl.withRoute("/foo", "foo").withRoute("/bar", "bar")
          .withRoute("/too", "too").route("/bar");
+        
+         /* LEVEL 1
+         String result = routerImpl.withRoute("/foo/*/bar/*", "yahoo").withRoute("/bar", "bar")
+         .withRoute("/too", "too").route("/foo/t/bar/p");
+         */
+        
+         /* LEVEL 2
+         String result = routerImpl.withRoute("/foo/:id/bar/:id1", "voila").withRoute("/bar", "bar")
+         .withRoute("/too/*", "too").route("/too/id");
+         */
          System.out.println(result);
     }
 }
